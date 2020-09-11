3f613b0307687f6c0a295eff7f4a9b254facd4fd
Jaap Kanbier <jaapkanbier@hotmail.com>
2020-09-11 12:40:10 +0200

    added comments and title

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
+// app state
 scoreBoard = {
     unicorn: false,
     giraffe: false
 }
 
+winDisplayed = false // to prevent alerts from poping up
+
 const main = () => {
     // https://ar-js-org.github.io/AR.js-Docs/ui-events/
+    // register event handler for scanning markers
     AFRAME.registerComponent('markerhandler', {
         init: function () {
             this.el.sceneEl.addEventListener('markerFound', (event) => {
+                // when market is scanned, pass the ID to the marker handler
                 handleMarkerEvent(event.target.id)
+                // also check total score to see if all markers have been scanned
                 checkTotalScore()
             })
         }
     });
-
 }
 
+// loop through all scores to see if non are missing
+// if so, user has won
 const checkTotalScore = () => {
     score = []
     for (const key in scoreBoard) {
@@ -24,16 +31,20 @@ const checkTotalScore = () => {
             score.push(value)
         }
     }
-    if (!score.includes(false)) {
-        alert('YOU WON')
+    if (!score.includes(false) && !winDisplayed) {
+        alert("You have won!")
+        winDisplayed = true
     }
 }
 
+// update UI based of DOMelement ID
 const changeScoreBoardUI = (htmlID) => {
     scoreEl = document.getElementById(htmlID)
     scoreEl.style.backgroundColor = "green";
 }
 
+// check which marker has been scanned
+// might get bloaty with many markers, could use refactoring
 const handleMarkerEvent = (markerID) => {
     switch (markerID) {
         case 'unicornMarker':
