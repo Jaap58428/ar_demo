01d70763d81159bd1c00d5f366c59cfd965eaa6d
Jaap Kanbier <jaapkanbier@hotmail.com>
2020-09-11 12:27:42 +0200

    marker event handling plus scoring

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
+scoreBoard = {
+    unicorn: false,
+    giraffe: false
+}
+
 const main = () => {
-    console.log('waddup');
-
-    giraffe = document.getElementById('giraffe')
-    giraffe.addEventListener('click', ()=>{
-        console.log('HELLO');
-    })
-
-
     // https://ar-js-org.github.io/AR.js-Docs/ui-events/
     AFRAME.registerComponent('markerhandler', {
         init: function () {
-            console.warn('WHATD');
             this.el.sceneEl.addEventListener('markerFound', (event) => {
-                console.log(event);
+                handleMarkerEvent(event.target.id)
+                checkTotalScore()
             })
         }
     });
 
+}
+
+const checkTotalScore = () => {
+    score = []
+    for (const key in scoreBoard) {
+        if (scoreBoard.hasOwnProperty(key)) {
+            const value = scoreBoard[key];
+            score.push(value)
+        }
+    }
+    if (!score.includes(false)) {
+        alert('YOU WON')
+    }
+}
+
+const changeScoreBoardUI = (htmlID) => {
+    scoreEl = document.getElementById(htmlID)
+    scoreEl.style.backgroundColor = "green";
+}
+
+const handleMarkerEvent = (markerID) => {
+    switch (markerID) {
+        case 'unicornMarker':
+            scoreBoard.unicorn = true
+            changeScoreBoardUI('unicornScore')
+            break;
+    
+        case 'giraffeMarker':
+            scoreBoard.giraffe = true
+            changeScoreBoardUI('giraffeScore')
+            break;
+    
+        default:
+            break;
+    }
 }
 
 window.onload = main
